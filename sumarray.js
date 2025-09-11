97f3ebc37ba29a15136b4e9e543b674ae2cc6660
Perslay <natalia.44.fedyk@gmail.com>
2025-09-11 20:59:40 +0200

    Array Sum 2
    Handling arrays that contain non-numeric elements.

--- sumarray.js
+++ sumarray.js
@@ -1,10 +1,13 @@
 // Write a function that takes an array of numbers and returns the sum of those numbers.
 
-let numbersArray = [];
+const numbersArray = [2, 5, 11];
+const isNumArray = (arr) => arr.every((item) => typeof item === "number");
 
 function sumArray(arr) {
   if (!Array.isArray(arr)) {
     return "Input is not an array.";
+  } else if (!isNumArray(arr)) {
+    return "Array contains non-numeric elements.";
   } else {
     return arr.reduce((sum, num) => sum + num, 0);
   }
