c849bcbf5a45dee6fc486b461ef7a57cb34f9b9a
Perslay <natalia.44.fedyk@gmail.com>
2025-11-16 18:07:01 +0100

    Login form tests
    Testing the login form by filling with various input and checking the expected results.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -23,3 +23,50 @@ describe("Get on commands/actions page and type in the input field", () => {
     cy.get(".action-email").should("have.value", "my@email.com");
   });
 });
+
+/*------------*/
+
+describe("Filling the form and logging in", () => {
+  it("Fills the form with correct input and submits it", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.get("button[type='submit']").click();
+    cy.get("#flash").should("contain", "You logged into a secure area!");
+  });
+  it("Fails to log in with empty fields", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("button[type='submit']").click();
+    cy.get("#flash").should("contain", "Your username is invalid!");
+  });
+  it("Fails to log in with wrong username and correct password", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("#username").type("wronguser");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.get("button[type='submit']").click();
+    cy.get("#flash").should("contain", "Your username is invalid!");
+  });
+  it("Fails to log in with correct username and wrong password", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("wrongpassword");
+    cy.get("button[type='submit']").click();
+    cy.get("#flash").should("contain", "Your password is invalid!");
+  });
+  it("Fails to log in with incorrect credentials", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("#username").type("wronguser");
+    cy.get("#password").type("wrongpassword");
+    cy.get("button[type='submit']").click();
+    cy.get("#flash").should("contain", "Your username is invalid!");
+  });
+  it("Logs out successfully after logging in", () => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.get("button[type='submit']").click();
+    cy.get("a[href='/logout']").click();
+    cy.get("#flash").should("contain", "You logged out of the secure area!");
+    cy.url().should("include", "/login");
+  });
+});
